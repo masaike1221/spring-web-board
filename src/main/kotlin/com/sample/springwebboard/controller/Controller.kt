3beb45d560ab24b38f1066eb69784946242ceeab
masahiro-ikeda <masahiro.ikeda@careritz.co.jp>
2019-01-19 16:47:20 +0900

    データソースを依存性注入できるよう書き換え

--- src/main/kotlin/com/sample/springwebboard/controller/Controller.kt
+++ src/main/kotlin/com/sample/springwebboard/controller/Controller.kt
@@ -2,7 +2,7 @@ package com.sample.springwebboard.controller
 
 import com.sample.springwebboard.model.entity.MessageEntity
 import com.sample.springwebboard.model.entity.MessageForm
-import com.sample.springwebboard.service.MessageService
+import com.sample.springwebboard.model.service.MessageService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.servlet.ModelAndView
 
 @Controller
-class Controller{
+class Controller {
     @Autowired
     lateinit var service: MessageService
 
--- src/main/kotlin/com/sample/springwebboard/model/service/MessageService.kt
+++ src/main/kotlin/com/sample/springwebboard/model/service/MessageService.kt
@@ -1,79 +1,56 @@
-package com.sample.springwebboard.service
+package com.sample.springwebboard.model.service
 
 import com.sample.springwebboard.model.dao.MessageDao
 import com.sample.springwebboard.model.entity.MessageEntity
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.sql.Connection
-import java.sql.DriverManager
 import java.sql.SQLException
 import java.sql.Timestamp
+import javax.sql.DataSource
 
 @Service
 class MessageService {
 
+    @Autowired
+    lateinit var dataSource: DataSource
+
     fun getMessage(): List<MessageEntity> {
 
-        var con: Connection? = null
-
         try {
-            con = getConnection()
-            val dao: MessageDao = MessageDao(con)
+            val dao: MessageDao = MessageDao(dataSource.connection)
             return dao.selectMessage()
 
         } catch (e: SQLException) {
-            println("log [error:getMessage]")
             e.printStackTrace()
-            throw RuntimeException()
-        } finally {
-            close(con!!)
+            throw RuntimeException("SQL: Select Error")
         }
     }
 
     fun postMessage(name: String, text: String) {
 
-        var con: Connection? = null
+        val con: Connection = dataSource.connection
+        con.autoCommit = false
 
         try {
-            con = getConnection()
-            con.autoCommit = false
 
             val dao: MessageDao = MessageDao(con)
-
             val message: MessageEntity = MessageEntity(
                     dao.getNextId(),
                     name,
                     text,
                     Timestamp(System.currentTimeMillis())
             )
-
             if (dao.insertMessage(message)) {
                 con.commit()
             } else {
                 con.rollback()
             }
+
         } catch (e: SQLException) {
-            println("log [error:postMessage]")
-            con!!.rollback()
             e.printStackTrace()
-            throw RuntimeException()
-        } finally {
-            close(con!!);
-        }
-    }
-
-    private fun getConnection(): Connection {
-
-        val url:  String = "jdbc:mysql://localhost/develop?useSSL=false"
-        val user: String = "root"
-        val pass: String = "pass"
-
-        Class.forName("com.mysql.jdbc.Driver")
-        return DriverManager.getConnection(url, user, pass)
-    }
-
-    private fun close(connection: Connection) {
-        if (!connection.isClosed) {
-            connection.close()
+            con.rollback()
+            throw RuntimeException("SQL: Insert Error")
         }
     }
 }
